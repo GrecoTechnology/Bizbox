c0b84421d91b9ad12f0fa320d5706fcdfbb6f171
Greco Technology <121058963+GrecoTechnology@users.noreply.github.com>
2023-05-01 14:19:32 -0400

    Update update.sh
    Added comments on what the code does.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Create an empty file called ansible-update.log or clear its content if it already exists
 printf '' > /srv/git/bizbox/ansible-update.log
+
+# Update Ansible to a specific version range with additional options
+# Redirect both error and regular messages to the ansible-update.log file
 python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade ansible">=6.0.0,<7.0.0" 2>&1 | tee -a /srv/git/bizbox/ansible-update.log
